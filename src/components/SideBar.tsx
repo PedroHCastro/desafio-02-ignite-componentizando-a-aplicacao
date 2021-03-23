514077ce56595bc1a39f8543d51c703e326ade2e
PedroHCastro <phcastro.contato@gmail.com>
2021-03-23 13:21:58 -0300

    complete test ignite-template-componentizando-a-aplicacao

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,32 @@
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }[],
+  onClick: Function,
+  selectedGenreId: number
+}
+
+export function SideBar({genres, onClick, selectedGenreId}:SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
+}
